a6a8f5c6152e59132f0b47302bcbb6779f364b98
SonuKM <sonumehta0983@gmail.com>
2025-08-14 13:50:36 +0530

    Added register validation for singup routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import { registerValidation } from "../validation/registerValidation.js";
 
 const router = express.Router();
 
-router.post("/signup", register);
+router.post("/signup", registerValidation, register);
 router.post("/login", login);
 
 export default router;
